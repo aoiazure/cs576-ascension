32bc1a9626f4bd5667fdb77fddcb978552c86b4f
khouiz <78759878+khouiz@users.noreply.github.com>
2021-12-01 05:31:13 -0500

    fix mainmenu conflict

--- Assets/Menu/MainMenu.cs
+++ Assets/Menu/MainMenu.cs
@@ -9,12 +9,8 @@ public class MainMenu : MonoBehaviour {
         Debug.Log("Exited game!");
     }
 
-<<<<<<< HEAD:Assets/MainMenu.cs
-    public void StartButton()
-    {
-=======
+
     public void StartButton() {
->>>>>>> 3ad6980d3bb8f6905cdc6e7f84e83d7fd43090c3:Assets/Menu/MainMenu.cs
         SceneManager.LoadScene("Ascension");
     }
 }
