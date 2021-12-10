0631737eaf56b7e784bff31e1f76210620150c9b
khouiz <78759878+khouiz@users.noreply.github.com>
2021-12-10 06:37:08 -0500

    enemy animation

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -10,27 +10,33 @@ public class Enemy : MonoBehaviour {
     public Transform goal;
     NavMeshAgent agent;
     public AudioClip death;
-    public AudioClip hit;
+    Animator anim;
+    bool walking;
+    bool hit;
 
     void Start() {
         gm.enemy_list.Add(this); // add reference to enemy to enemy_list for later
         agent = GetComponent<NavMeshAgent>();
         agent.destination = goal.position;
+        anim = GetComponentInChildren<Animator>();
+        anim.SetBool("walking", true);
+        anim.SetBool("hit", false);
     }
 
     private void FixedUpdate() {
         agent.destination = goal.position;
+        anim.SetBool("walking", true);
     }
 
     public void TakeDamage(float amount) {
         health -= amount;
+        anim.Play("hit", 0, 0);
         if (health <= 0f) {
             Die();
         }
     }
 
     void Die() {
-        
         gm.EnemyDie();
         AudioSource.PlayClipAtPoint(death, transform.position);
         gm.enemy_list.Remove(this);
