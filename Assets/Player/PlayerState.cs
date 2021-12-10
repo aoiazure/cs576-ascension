be8145a37e7759b2333ebbfdd00dadc25fc32fc3
Norapat Rerngkasetkig <nrerngkasetk@umass.edu>
2021-12-10 02:26:30 -0500

    Added UI to show Wave number and Powerup duration.
    Fixed bug with game timer.

--- Assets/Player/PlayerState.cs
+++ Assets/Player/PlayerState.cs
@@ -1,24 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerState : MonoBehaviour {
-
+    // Refs
     public GameManager gm;
+    public GameObject powerup_time_text;
 
+    // Powers
     int current_power = -1;
     float power_timer = 0.0f;
     const float power_duration = 10.0f;
 
-    float old_gun_damage;
-    float old_player_speed;
-    float old_enemy_speed;
 
     Gun gun;
 
     // Start is called before the first frame update
     void Start() {
         gun = GetComponentInChildren<Gun>();
+        powerup_time_text.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,15 +27,17 @@ public class PlayerState : MonoBehaviour {
         // Do stuff if in a power
         if (current_power >= 0) {
             power_timer += Time.deltaTime;
+            // Adjust timer UI
+            Text t = powerup_time_text.GetComponent<Text>();
             switch (current_power) {
                 case 0: // damage
-
+                    t.text = "Damage Powerup: " + (10.0f - power_timer).ToString("F1") + "s";
                     break;
                 case 1: // speed
-
+                    t.text = "Firerate Powerup: " + (10.0f - power_timer).ToString("F1") + "s";
                     break;
                 case 2: // slow
-
+                    t.text = "Slow Powerup: " + (10.0f - power_timer).ToString("F1") + "s";
                     break;
                 default:
                     break;
@@ -58,11 +61,10 @@ public class PlayerState : MonoBehaviour {
                 }
                 current_power = -1; // reset to no power
                 power_timer = 0.0f; // reset timer
+                powerup_time_text.SetActive(false);
             }
 
         }
-
-
     }
 
     public void ChangePower(int power) {
@@ -87,20 +89,25 @@ public class PlayerState : MonoBehaviour {
             // apply new power
             current_power = power;
             power_timer = 0.0f;
+            Text t = powerup_time_text.GetComponent<Text>();
             switch (current_power) {
                 case 0: // damage
+                    t.text = "Damage Powerup: " + (10.0f - power_timer);
                     gun.damage *= 2;
                     break;
                 case 1: // speed
+                    t.text = "Firerate Powerup: " + (10.0f - power_timer);
                     gun.firerate *= 2;
                     break;
                 case 2: // slow
+                    t.text = "Slow Powerup: " + (10.0f - power_timer);
                     // slow enemies
                     gm.EnableSlow(true);
                     break;
                 default:
                     break;
             }
+            powerup_time_text.SetActive(true);
         } else {
             power_timer = 0.0f;
         }
--- Assets/Scenes/GameManager.cs
+++ Assets/Scenes/GameManager.cs
@@ -14,6 +14,8 @@ public class GameManager : MonoBehaviour {
     float game_timer = 0.0f;
     int total_time_elapsed = 0;
 
+    public GameObject wave_count_text;
+
     public Zone[] zones;
     Zone current_zone = null;
 
@@ -38,7 +40,7 @@ public class GameManager : MonoBehaviour {
     float timer_waves = 0.0f;
 
     int num_enemies = 0;
-    int max_num_enemies; // max num enemies = wave * 2 (min 3)
+    int max_num_enemies = 3; // max num enemies = wave * 2 (min 3)
     float spawn_delay = 2.0f;
     float spawn_timer = 0.0f;
 
@@ -61,16 +63,13 @@ public class GameManager : MonoBehaviour {
         if (game_timer >= 1.0f) {
             game_timer = 0.0f;
             total_time_elapsed += 1;
-            string minutes = ((total_time_elapsed / (total_time_elapsed % 60)) - 1).ToString(), seconds;
+            string minutes = (total_time_elapsed / 60).ToString(), seconds;
             if (total_time_elapsed % 60 < 10)
                 seconds = "0" + (total_time_elapsed % 60);
             else
                 seconds = (total_time_elapsed % 60).ToString();
             game_time_text.text = "Time:\n" + minutes + ":" + seconds;
         }
-
-        // set max number of enemies per wave = wave * 2 (min 3)
-        max_num_enemies = Mathf.Max(wave_number * 2, 3);
 
         // IF IN A ZONE
         if (current_zone != null) {
@@ -83,12 +82,16 @@ public class GameManager : MonoBehaviour {
                 timer_waves = 0.0f;
                 // tick up wave count
                 wave_number += 1;
+                // set max number of enemies per wave = wave * 2 (min 3)
+                max_num_enemies = Mathf.Max(wave_number * 2, 3);
                 // scale time between waves slowly
                 time_between_waves += 2.0f;
                 // scale spawn delay with wave number
                 spawn_delay = (time_between_waves - 2) / max_num_enemies;
                 // reset number of enemies
                 num_enemies = 0;
+                // Update Wave UI on screen
+                StartCoroutine(UpdateWaveText());
             }
 
             //// ENEMY SPAWNS
@@ -128,16 +131,35 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    // helpers
+    //// Helpers
+    
+    IEnumerator UpdateWaveText() {
+        Text t = wave_count_text.GetComponent<Text>();
+        Color c = t.color;
+        c.a = 1f;
+        t.color = c;
+        t.text = "Wave " + wave_number;
+        wave_count_text.SetActive(true);
+        yield return new WaitForSeconds(3.0f);
+        for (float alpha = 1f; alpha >= 0; alpha -= 0.1f) {
+            c.a = alpha;
+            t.color = c;
+            yield return null;
+        }
+        wave_count_text.SetActive(false);
+    }
+
+    // Called when an Enemy dies
     public void EnemyDie() {
         kill_count += 1;
         kill_tracker.text = "Kills: " + kill_count;
     }
+    // Called when Player enters/exits a Zone
     public void SetCurrentZone(Zone z = null) {
         current_zone = z;
         game_start = true;
     }
-
+    // Enable slow Powerup across all active enemies
     public void EnableSlow(bool active) {
         if (active) { // if active, slow everything right now
             foreach (Enemy e in enemy_list) {
